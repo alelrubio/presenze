37ac7bec046e3e0eaab2db31e1e87b240b5a8157
alelrubio <alelrubio20@gmail.com>
2016-11-10 17:21:20 +0100

    Add files via upload

--- src/assets/services/srv.backend.js
+++ src/assets/services/srv.backend.js
@@ -13,6 +13,12 @@ module.exports = function (ngModule) {
                     return
                 };
 
+/*
+console.log("method: " + callParams.action.method);
+console.log("url: " + callParams.action.url);
+console.log("data: " + callParams.data);
+*/
+                    console.log('call BACKEND');
                 const deferred = $q.defer();
                 $http({
                     method: callParams.action.method,
