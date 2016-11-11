7ec42753635f443d4abe7b2bea5335b38d23d7eb
alelrubio <alelrubio20@gmail.com>
2016-11-11 17:28:56 +0100

    Add files via upload

--- src/constants/lang.js
+++ src/constants/lang.js
@@ -6,9 +6,12 @@ module.exports = function (ngModule) {
 
     ngModule.constant('lang', {
         "errors": {
+        	"usernameNotValid": "Username non valido, valori ammessi sono lettere, numeri ed i caratteri '.', '-' e '_'",
             "passwordNotValid": "Password non valida, inserire almeno un carattere Minuscolo, un carattere Maiuscolo ed una cifra",
 
+			"emailNotValid": "Email non corretta",
 
+			"nameNotValid": "Valore inserito non valido"
         }
     })
 };
